0288c22cf074e0f456b76730fb6b5645aefe23e1
sabiq mohammed <sabiq.mohammed4@gmail.com>
2021-12-28 15:08:11 +0530

    list

--- list.py
+++ list.py
@@ -1,3 +1,10 @@
 family = ["Mom","dad","sis1","sis2"]
+fam = family+[1,2,3,3,]
+string = "sabiq"
+st = string+" mohammed"
+app = family.append(input("enter the value to insert "))
+
+
+
 
 print(family)
