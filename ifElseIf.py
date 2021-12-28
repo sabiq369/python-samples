928a11281daa2d728c79aa3a10a9711b1c297467
sabiq mohammed <sabiq.mohammed4@gmail.com>
2021-12-28 15:44:17 +0530

    ifElseIf

---
+++ ifElseIf.py
@@ -0,0 +1,13 @@
+num1 = int(input("Enter a number :"))
+if num1>0 :
+    print("You have enterd a postive number")
+elif num1 == 0 :
+    print("please enter a number otherthan zero")   
+    num1 = int(input("Enter a number"))
+    if num1>0 :
+     print("You have enterd a postive number")
+    else :
+     print("You have enterd a negative number")   
+
+else :
+     print("You have enterd a negative number")   
