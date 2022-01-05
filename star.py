b21e2f1008584a683fbea9b05c030892c1ed8587
sabiq mohammed <sabiq.mohammed4@gmail.com>
2022-01-05 17:46:04 +0530

    star

---
+++ star.py
@@ -0,0 +1,115 @@
+# row = int(input("Enter how many rows of star you want to print a square : "))
+# for i in range(row):
+#     for j in range(row):
+#         print("* ",end=" ")
+#     print()    
+
+# -------------------------------------------------------------------------------------------------
+
+# row = int(input("Enter the no of rows : "))
+# for i in range(row):
+#     for j in range(i+1):
+#         print("* ",end="")
+#     print()    
+
+#---------------------------------------------------------------------------------------------------
+
+# row = int(input("Enter a number : "))
+# for i in range(row):
+#     for j in range(i,row):
+#         print("* ",end="")
+#     print()
+
+#---------------------------------------------------------------------------------------------------
+
+# row = int(input("Enter a number : "))
+# for i in range(row):
+#     for j in range(i,row):
+#         print(" ",end=" ")
+#     for j in range(i+1):
+#          print("* ",end="") 
+#     print()      
+
+#---------------------------------------------------------------------------------------------------
+
+# row = int(input("Enter a number : "))
+# for k in range(row):
+ 
+#  for i in range(row) :
+#     print(end="")
+#     for j in range(i,row) :
+#         print(" ",end="")
+#     for j in range(i) :
+#         print("*",end="")
+#     for j in range(i+1) :
+#         print("*",end="")
+#     for j in range(i,row) :
+#         print(" ",end="")    
+#     for j in range(i,row) :
+#         print(" ",end="")
+#     for j in range(i) :
+#         print("*",end="")
+#     for j in range(i+1) :
+#         print("*",end="")
+               
+       
+#     print()          
+
+#---------------------------------------------------------------------------------------------------
+
+# row = int(input("Enter a number : "))
+# for i in range(row) :
+#     for j in range(i+1) :
+#         print(" ",end="")
+#     for j in range(i,row) :
+#         print("*",end="")
+#     for j in range (i,row-1) :
+#         print("*",end="")  
+#     print()    
+
+#---------------------------------------------------------------------------------------------------
+
+# row = int(input("Enter a number : "))
+# for i in range(row-1) :
+#     for j in range(i,row) :
+#         print(" ",end="")
+#     for j in range(i) :
+#         print("*",end="")
+#     for j in range(i+1) :
+#         print("*",end="")    
+#     print()        
+# for i in range(row) :
+#     for j in range(i+1) :
+#         print(" ",end="")
+#     for j in range(i,row-1) :
+#         print("*",end="")   
+#     for j in range(i,row)  :
+#         print("*",end="")      
+#     print()    
+
+#---------------------------------------------------------------------------------------------------
+
+# row = int(input("Enter a number : "))
+# for i in range(row-1) :
+#     for j in range(i+1) :
+#         print("*",end="")
+#     for j in range(i,row) :
+#         print(" ",end="") 
+#     for j in range(i,row) :
+#         print(" ",end="")    
+#     for j in range(i+1) :
+#         print("*",end="")    
+#     print()
+# for i in range(row) :
+#     for j in range(i,row) :
+#         print("*",end="")   
+#     for j in range(i+1) :
+#         print(" ",end="")   
+#     for j in range(i+1) :
+#         print(" ",end="")
+#     for j in range(i,row) :
+#         print("*",end="")        
+#     print()    
+
+#---------------------------------------------------------------------------------------------------
+
